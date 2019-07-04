e56b5289c6a9561d119769f94f0dd6547a2a5830
Savidya Sathyani <savidya19@gmail.com>
2019-07-04 16:48:47 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -1,35 +1,70 @@
 import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
-import Intro from "./Intro";
+import Header from "./components/Header";
+import Grid from "./components/Grid";
+import Form from "./components/Form";
+
+const styles = {
+  textAlign: "center",
+  margin: 10,
+  padding: 20,
+  FontFamily: "sans-serif"
+};
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      name: "Manny"
+      notes: [
+        {
+          id: 1,
+          title: "Add Course notes",
+          details: "sdgfjdsfjhdsgfjhdsgfjhd jsdhgfjsdfggjgdjhfg"
+        },
+        {
+          id: 2,
+          title: "Add Course notes 2",
+          details: "sdgfjdsfjhdsgfjhdsgfjhd jsdhgfjs fdgfg dfggjgdjhfg"
+        },
+        {
+          id: 3,
+          title: "Add Course notes 3",
+          details: "sdgfjdsfjhdsgfjhdsgfjhd jsdhgfjsdffgf gfgfgg gjgdjhfg"
+        }
+      ],
+      name: "Manny",
+      currentTitle: "",
+      currentdetails: ""
     };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
-  componentDidMount() {
-    console.log("component just loaded");
+  handleChange(event) {
+    const name = event.target.name;
+    const value = event.target.value;
+
+    this.setState({
+      [name]: value
+    });
+  }
+
+  handleSubmit(event) {
+    alert(`Your note ${this.state.currentTitle} has been added.`);
+    event.preventDefault();
   }
 
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <Intro name={this.state.name} />
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className={styles}>
+        <Header name={this.state.name} />
+        <Form
+          currentTitle={this.state.currentTitle}
+          currentdetails={this.state.currentdetails}
+          handleChange={this.handleChange}
+          handleSubmit={this.handleSubmit}
+        />
+        <Grid notes={this.state.notes} />
       </div>
     );
   }
---
+++ src/components/Form.js
@@ -0,0 +1,51 @@
+import React from "react";
+
+const styles = {
+  margin: 10
+};
+
+class Form extends React.Component {
+  handleChange = event => {
+    this.props.handleChange(event);
+  };
+  handleSubmit = event => {
+    this.props.handleSubmit(event);
+  };
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit} className="col s12">
+        <div className="row" style={styles}>
+          <div className="input-feild col s3">
+            <input
+              name="currentTitle"
+              type="text"
+              value={this.props.currentTitle}
+              onChange={this.props.handleChange}
+              placeholder="Title"
+            />
+          </div>
+          <div className="input-feild col s7">
+            <input
+              name="currentdetails"
+              value={this.props.currentDetails}
+              onChange={this.handleChange}
+              placeholder="Details"
+            />
+          </div>
+
+          <div className="input-feild col s2">
+            <button
+              className="btn-large waves-effect waves-light"
+              type="submit"
+              name="action"
+            >
+              Add Note{" "}
+            </button>
+          </div>
+        </div>
+      </form>
+    );
+  }
+}
+
+export default Form;
